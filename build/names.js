66b82e4b1de2e24b15dbaef09b82aae7b6d3c6c9
Luke Karrys <lukekarrys@gmail.com>
2017-03-16 12:05:54 -0700

    match names

---
+++ build/names.js
@@ -0,0 +1,46 @@
+const utils = require('scores/test/utils')
+const _ = require('lodash')
+const getBracket = require('../lib/index')
+
+/* Usage
+  node build/names.js \
+    ncaam \
+    2017 \
+    http://www.espn.com/mens-college-basketball/scoreboard/_/group/100/date/20170316 \
+    http://www.espn.com/mens-college-basketball/scoreboard/_/group/100/date/20170317
+ */
+const [sport, year, ...urls] = process.argv.slice(2)
+
+const parseUrl = (url) => new Promise((resolve, reject) => {
+  utils.parseUrl(url, {}, (err, events) => {
+    if (err) return reject(err)
+    return resolve(events)
+  })
+})
+
+// Get array of all teams from local bracket data
+const data = getBracket({sport, year})
+const teams = data.constants.REGION_IDS.reduce((acc, id) => {
+  acc.push(...data.bracket.regions[id].teams)
+  return acc
+}, []).sort()
+
+Promise.all(urls.map(parseUrl))
+  .then((events) => _.flatten(events).reduce((acc, event) => {
+    acc.push(...event.home.names)
+    acc.push(...event.away.names)
+    return acc
+  }, []).sort())
+  .then((all) => {
+    const missing = _.without(teams, ...all)
+    if (missing.length) {
+      console.log(JSON.stringify(all, null, 2))
+      missing.forEach((team) => {
+        console.log(team)
+        console.log(all.filter((a) => a.startsWith(team)))
+        console.log('-'.repeat(80))
+      })
+    } else {
+      console.log('All good')
+    }
+  })
